e22238b75057e3a4f74f98b4bc7598d57cb79ed6
je-pa <jieun0502@gmail.com>
2024-08-24 00:06:45 +0900

    [#3] ✅ 회원 테스트 코드 작성

--- src/test/java/com/ecommerce/api/service/auth/AuthServiceTest.java
+++ src/test/java/com/ecommerce/api/service/auth/AuthServiceTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.times;
 import com.ecommerce.IntegrationTestSupport;
 import com.ecommerce.api.controller.ApiResponse;
 import com.ecommerce.api.controller.auth.dto.request.MailRequest;
+import com.ecommerce.api.controller.auth.dto.request.SignupRequest;
 import com.ecommerce.domain.auth.repository.EmailAuthCodeRepository;
 import com.ecommerce.domain.member.entity.Member;
 import com.ecommerce.domain.member.repository.MemberRepository;
@@ -88,10 +89,109 @@ class AuthServiceTest extends IntegrationTestSupport {
         .hasMessage("중복된 이메일 입니다.");
   }
 
+  @DisplayName("회원가입을 진행한다.")
+  @Test
+  void signup(){
+    // given
+    String email = "test@email.com";
+    String authCode = "123456";
+    SignupRequest signupRequest = getSignupRequest(email, authCode);
+    emailAuthCodeRepository.setWithDurationByKey(myEncoder.encrypt(email), authCode);
+
+    // when
+    ApiResponse<String> signup = authService.signup(signupRequest);
+
+    // then
+    assertThat(signup)
+        .extracting("code", "status", "message", "data")
+        .contains(200, HttpStatus.OK, "OK", "회원가입이 완료되었습니다.");
+  }
+
+  @DisplayName("비밀번호와 비밀번호 확인을 동일하게 입력하지 않으면 회원가입을 진행할 수 없다.")
+  @Test
+  void signupWithPasswordMismatch(){
+    String email = "test@email.com";
+    String authCode = "123456";
+    String name = "홍길동";
+    String password = "Password1!";
+    String confirmPassword = "Password2!";
+    String tellNumber = "01077777777";
+    String address = "서울시 용산구 333";
+    SignupRequest signupRequest = new SignupRequest(
+        name, email, password, confirmPassword, tellNumber, address, authCode);
+    emailAuthCodeRepository.setWithDurationByKey(myEncoder.encrypt(email), authCode);
+
+    // when
+    assertThatThrownBy(() -> authService.signup(signupRequest))
+        .isInstanceOf(CustomException.class)
+        .hasMessage("두 비밀번호가 일치하지 않습니다.");
+  }
+
+  @DisplayName("이미 가입된 이메일은 회원가입을 진행할 수 없다.")
+  @Test
+  void signupWithEmailAlreadyExists(){
+    // given
+    String email = "test@email.com";
+    String authCode = "123456";
+    SignupRequest signupRequest = getSignupRequest(email, authCode);
+    emailAuthCodeRepository.setWithDurationByKey(myEncoder.encrypt(email), authCode);
+    memberRepository.save(createMember(email));
+
+    // when
+    // then
+    assertThatThrownBy(() -> authService.signup(signupRequest))
+        .isInstanceOf(CustomException.class)
+        .hasMessage("중복된 이메일 입니다.");
+  }
+
+  @DisplayName("인증 코드가 발급되지 않았거나 만료가 되었다면 회원가입이 진행되지 않는다.")
+  @Test
+  void signupWithCodeExpiredOrInvalid(){
+    // given
+    String email = "test@email.com";
+    String authCode = "123456";
+    SignupRequest signupRequest = getSignupRequest(email, authCode);
+
+    // when
+    // then
+    assertThatThrownBy(() -> authService.signup(signupRequest))
+        .isInstanceOf(CustomException.class)
+        .hasMessage("인증 코드가 만료되었거나 존재하지 않습니다.");
+  }
+
+  @DisplayName("인증코드가 일치하지 않으면 회원가입을 진행할 수 없다.")
+  @Test
+  void signupWithVerificationCodeMismatch(){
+    // given
+    String email = "test@email.com";
+    String authCode = "123456";
+    String savedAuthCode = "123457";
+
+    SignupRequest signupRequest = getSignupRequest(email, authCode);
+    emailAuthCodeRepository.setWithDurationByKey(myEncoder.encrypt(email), savedAuthCode);
+
+    // when
+    // then
+    assertThatThrownBy(() -> authService.signup(signupRequest))
+        .isInstanceOf(CustomException.class)
+        .hasMessage("인증코드가 일치하지 않습니다.");
+  }
+
+  private static SignupRequest getSignupRequest(String email, String authCode) {
+    String name = "홍길동";
+    String password = "Password1!";
+    String confirmPassword = "Password1!";
+    String tellNumber = "01077777777";
+    String address = "서울시 용산구 333";
+    SignupRequest signupRequest = new SignupRequest(
+        name, email, password, confirmPassword, tellNumber, address, authCode);
+    return signupRequest;
+  }
+
   private Member createMember(String email){
     String name = "박땡땡";
-    String password = "password123@";
-    String tellNumber = "01022221111";
+    String password = "Password123@";
+    String tellNumber = "010-2222-1111";
     String address = "서울특별시 송파구 올림픽로 240 여기동 어디게호"; // 암호화 길이가 108이 된걸로 확인됨.
     Role role = Role.GENERAL;
     return Member.builder()

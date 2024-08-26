ccc07e36e2297c98ceb1d83ab4add185f0af9e3c
je-pa <jieun0502@gmail.com>
2024-08-27 02:04:30 +0900

    [#7] 💡 위시리스트 주석

--- src/main/java/com/ecommerce/api/controller/wishlist/WishlistController.java
+++ src/main/java/com/ecommerce/api/controller/wishlist/WishlistController.java
@@ -29,12 +29,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class WishlistController {
   private final WishlistService wishlistService;
 
+  /**
+   * 상품 옵션을 선택하여 위시리스트를 등록합니다.
+   * @param memberId 로그인 유저 id
+   * @param requests 옵션 id 와 수량을 담은 dto
+   * @return 결과
+   */
   @PostMapping
   public ResponseEntity<ApiResponse<String>> set(
       @CurrentMemberId Long memberId, @RequestBody List<CreateWishlistRequest> requests){
     return ResponseEntity.ok(wishlistService.modify(memberId, requests));
   }
 
+  /**
+   * 위시리스트를 조회합니다.
+   * @param currentMemberId 현재 로그인 유저 id
+   * @param memberId 조회하려는 멤버 id
+   * @return 조회된 위시리스트
+   */
   @GetMapping
   public ResponseEntity<List<WishlistWithItemsResponse>> getList(
       @CurrentMemberId Long currentMemberId, @RequestParam Long memberId
@@ -42,6 +54,13 @@ public class WishlistController {
     return ResponseEntity.ok(wishlistService.readList(currentMemberId, memberId));
   }
 
+  /**
+   * 위시리스트 항목을 수정합니다.
+   * @param currentMemberId 현재 로그인 유저 id
+   * @param itemId 수정하려는 위시리스트 항목 id
+   * @param request 수정하려는 행위
+   * @return 결과
+   */
   @PatchMapping("/items/{itemId}/quantity")
   public ResponseEntity<ApiResponse<String>> updateItemQuantity(
       @CurrentMemberId Long currentMemberId,

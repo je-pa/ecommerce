965d542c1fb01de05372c0fb4af258aac1523b12
je-pa <76720692+je-pa@users.noreply.github.com>
2024-08-29 14:06:20 +0900

    [#9] ✅ order scheduler 테스트 코드 수정 (#11)

--- src/test/java/com/ecommerce/global/scheduler/OrderSchedulerTest.java
+++ src/test/java/com/ecommerce/global/scheduler/OrderSchedulerTest.java
@@ -4,19 +4,30 @@
  */
 package com.ecommerce.global.scheduler;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.ecommerce.IntegrationTestSupport;
 import com.ecommerce.domain.member.entity.Member;
 import com.ecommerce.domain.member.repository.MemberRepository;
 import com.ecommerce.domain.member.type.Role;
+import com.ecommerce.domain.order.entity.Order;
+import com.ecommerce.domain.order.entity.OrderItem;
 import com.ecommerce.domain.order.repository.OrderItemRepository;
 import com.ecommerce.domain.order.repository.OrderRepository;
 import com.ecommerce.domain.order.type.OrderStatus;
+import com.ecommerce.domain.product.entity.Product;
+import com.ecommerce.domain.product.entity.ProductOption;
 import com.ecommerce.domain.product.repository.ProductOptionRepository;
 import com.ecommerce.domain.product.repository.ProductRepository;
+import com.ecommerce.domain.product.type.OptionType;
+import com.ecommerce.domain.product.type.ProductCategory;
+import com.ecommerce.domain.store.entity.Store;
 import com.ecommerce.domain.store.repository.StoreRepository;
 import com.ecommerce.domain.wishlist.repository.WishlistItemRepository;
 import com.ecommerce.domain.wishlist.repository.WishlistRepository;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -57,8 +68,6 @@ class OrderSchedulerTest extends IntegrationTestSupport {
 
   @AfterEach
   void tearDown() {
-    wishlistItemRepository.deleteAllInBatch();
-    wishlistRepository.deleteAllInBatch();
     orderItemRepository.deleteAllInBatch();
     productOptionRepository.deleteAllInBatch();
     productRepository.deleteAllInBatch();
@@ -89,9 +98,49 @@ class OrderSchedulerTest extends IntegrationTestSupport {
         createdDateTime, LocalDateTime.of(2024, 8, 27, 23, 59));
     insertOrder(member1.getId(), OrderStatus.RETURNED,
         createdDateTime, LocalDateTime.of(2024, 8, 30, 0, 0));
+
+    LocalDateTime dateTime = LocalDateTime.of(2024, 8, 30, 0, 0);
+    List<Order> orders = orderRepository.findByStatusAndModifiedDateTimeBefore(
+        OrderStatus.REQUESTED_RETURN, dateTime.minusDays(1));
+    int product1Option1Quantity = 100;
+    int product1Option2Quantity = 200;
+    int totalQuantity = 300;
+    Product product = productRepository.save(createProduct("상품1", totalQuantity));
+    ProductOption product1Option1 = createProductOption("옵션1-1", product, product1Option1Quantity);
+    ProductOption product1Option2 = createProductOption("옵션1-2", product, product1Option2Quantity);
+    productOptionRepository.saveAll(List.of(product1Option1, product1Option2));
+    int totalOrderItemProduct1Option1Quantity = 0;
+    int totalOrderItemProduct1Option2Quantity = 0;
+    List<OrderItem> orderItems = new ArrayList<>();
+    for(Order order : orders){
+      orderItems.add(createOrderItem(order, product1Option1, 10));
+      totalOrderItemProduct1Option1Quantity += 10;
+      orderItems.add(createOrderItem(order, product1Option2, 20));
+      totalOrderItemProduct1Option2Quantity += 20;
+    }
+    orderItemRepository.saveAll(orderItems);
+    int totalOrderItemProduct1Quantity = totalOrderItemProduct1Option1Quantity + totalOrderItemProduct1Option2Quantity;
+
+
     // when
-    orderScheduler.processReturnedOrders(
-        LocalDateTime.of(2024, 8, 30, 0, 0));
+    orderScheduler.processReturnedOrders(dateTime);
+
+    // then
+    assertThat(productRepository.findById(product.getId()))
+        .isNotEmpty()
+        .get()
+        .extracting("name", "stockQuantity")
+        .contains("상품1", totalQuantity + totalOrderItemProduct1Quantity);
+
+  }
+
+  private OrderItem createOrderItem(Order order, ProductOption option, int quantity){
+    return OrderItem.builder()
+        .order(order)
+        .price(option.getPrice())
+        .option(option)
+        .quantity(quantity)
+        .build();
   }
 
   // AuditingEntityListener 관계없이 실행되게 jdbc를 사용
@@ -100,6 +149,36 @@ class OrderSchedulerTest extends IntegrationTestSupport {
         "VALUES (?, ?, ?, ?, ?)";
 
     jdbcTemplate.update(sql, memberId, status.toString(), 10000, createdDateTime, modifiedDateTime);
+  }
+
+  private ProductOption createProductOption(String name, Product product, int quantity) {
+    return ProductOption.builder()
+        .product(product)
+        .count(quantity)
+        .name(name)
+        .optionType(OptionType.MANDATORY)
+        .price(10000)
+        .build();
+  }
+
+  private Product createProduct(String name, int quantity) {
+    return Product.builder()
+        .name(name)
+        .stockQuantity(quantity)
+        .store(storeRepository.save(createStore("storeName1")))
+        .category(ProductCategory.TOP)
+        .info("상품정보")
+        .thumbnailImgUrl("url")
+        .price(10000)
+        .build();
+  }
+
+  private Store createStore(String name){
+    return Store.builder()
+        .name(name)
+        .tellNumber("encrypted")
+        .info("정보")
+        .build();
   }
 
   private Member createMember(String email){
